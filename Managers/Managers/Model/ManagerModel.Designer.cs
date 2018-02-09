0aa16f868712f5d92918a7808f9d3932a5ff9bc4
Christie <me.mcbeukman@gmail.com>
2018-02-09 08:17:50 +0200

    Changed path

--- Managers/Managers/Model/ManagerModel.Designer.cs
+++ Managers/Managers/Model/ManagerModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Documents\Visual Studio 2017\Projects\GitLab\managers\Managers\Managers\Model\ManagerModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Documents\Visual Studio 2017\Projects\GitLab\SmartManager\Managers\Managers\Model\ManagerModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
