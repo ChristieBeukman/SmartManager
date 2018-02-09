5a94b87cca1d4bc276c5043e47a0289a06fba875
Christie <me.mcbeukman@gmail.com>
2018-02-09 15:00:11 +0200

    AddWorking

---
+++ Managers/Managers/Services/IDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Managers.Model;
+
+namespace Managers.Services
+{
+    public interface IDataAccess
+    {
+        ObservableCollection<AccountType> GetAccountTypes();
+
+        void AddAccount(Account a);
+        void UpdateAccount(Account a);
+        void DeleteAccount(Account a);
+        ObservableCollection<Account> GetAccounts();
+
+    }
+}
