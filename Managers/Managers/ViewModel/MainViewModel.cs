f9e4d227054072837ad8af14d32370c6b8132d47
Christie <me.mcbeukman@gmail.com>
2018-02-09 14:24:47 +0200

    Styless

--- Managers/Managers/ViewModel/MainViewModel.cs
+++ Managers/Managers/ViewModel/MainViewModel.cs
@@ -1,34 +1,44 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Managers.ViewModel.Account;
 
 namespace Managers.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private ViewModelBase _CurrentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return _CurrentViewModel;
+            }
+
+            set
+            {
+                _CurrentViewModel = value;
+                RaisePropertyChanged("CurrentViewModel");
+            }
+        }
+
+        #region CurrentViewModel
+        private static readonly AccountManagementViewModel accountManagementViewModel = new AccountManagementViewModel();
+
+        public RelayCommand ViewAccountManagementCommand { get; set; }
+
+        void ExecuteViewAccountManagement()
+        {
+            CurrentViewModel = MainViewModel.accountManagementViewModel;
+        }
+
+        #endregion
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            ViewAccountManagementCommand = new RelayCommand(ExecuteViewAccountManagement);
         }
     }
 }
